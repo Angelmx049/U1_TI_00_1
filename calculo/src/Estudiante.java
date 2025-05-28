bcfbe5869e22cdbca3b06382730dba61a9066977
Mujica Aguilar Angel <20243ds049@utez.edu.mx>
2025-05-27 23:13:39 -0600

    Este es el codigo final

---
+++ calculo/src/Estudiante.java
@@ -0,0 +1,31 @@
+package Model;
+public class Estudiante {
+
+        private String nombre;
+        private String matricula;
+        private double[] notas = new double[5];
+
+        public String getNombre() {
+                return nombre;
+        }
+
+        public void setNombre(String nombre) {
+                this.nombre = nombre;
+        }
+
+        public String getMatricula() {
+                return matricula;
+        }
+
+        public void setMatricula(String matricula) {
+                this.matricula = matricula;
+        }
+
+        public double getNota(int posicion) {
+                return this.notas[posicion];
+        }
+
+        public void setNota(int posicion, double valor){
+                this.notas[posicion]= valor;
+        }
+}
